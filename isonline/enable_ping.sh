9a73e364304f4350286cd81c872eb9b5e749a550
Nicholas Christopoulos <nereus@freemail.gr>
2019-11-30 22:57:25 +0200

    quick and dirty fix

--- isonline/enable_ping.sh
+++ isonline/enable_ping.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
 sysctl -w net.ipv4.ping_group_range="0 2000"
+if [ $(id -u) -eq 0 ]; then
+	echo "net.ipv4.ping_group_range=0 2000" >> /etc/sysctl.conf
+	echo "net.ipv6.ping_group_range=0 2000" >> /etc/sysctl.conf
+	sysctl -p
+fi
+
