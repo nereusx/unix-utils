ac3c203c31c42b0d84fa8c58a10dc3a6c21b3d88
Nicholas Christopoulos <nereus@freemail.gr>
2019-12-16 11:01:36 +0200

    quick and dirty fix

--- dof/list.c
+++ dof/list.c
@@ -1,3 +1,23 @@
+/*
+ *	Dynamic Single-linked List module
+ * 
+ *	Copyright (C) 2017-2020 Free Software Foundation, Inc.
+ *
+ *	This is free software: you can redistribute it and/or modify it under
+ *	the terms of the GNU General Public License as published by the
+ *	Free Software Foundation, either version 3 of the License, or (at your
+ *	option) any later version.
+ *
+ *	It is distributed in the hope that it will be useful, but WITHOUT ANY
+ *	WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *	FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *	for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with it. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * 	Written by Nicholas Christopoulos <nereus@freemail.gr>
+ */
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
