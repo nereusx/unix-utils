fec43bdcc28e5954d0e62c84e7907f656c98114c
Nicholas Christopoulos <nereus@freemail.gr>
2019-12-16 11:06:57 +0200

    quick and dirty fix

--- pathpp/pathpp.c
+++ pathpp/pathpp.c
@@ -1,12 +1,25 @@
 /*
- * check directories (exists,is_dir,!duplicate)
- * and/or add directories to a path
+ *	path++ check's directories (if exist, if is_dir, if !duplicate) and/or
+ *	add directories to a path.
+ *
+ *	Copyright (C) 2017-2020 Free Software Foundation, Inc.
+ *
+ *	This is free software: you can redistribute it and/or modify it under
+ *	the terms of the GNU General Public License as published by the
+ *	Free Software Foundation, either version 3 of the License, or (at your
+ *	option) any later version.
+ *
+ *	It is distributed in the hope that it will be useful, but WITHOUT ANY
+ *	WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *	FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *	for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with it. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * 	Written by Nicholas Christopoulos <nereus@freemail.gr>
  * 
- * nicholas christopoulos (nereus@freemail.gr)
- *
- * this software is released under GPLv3 or newer license
- *
- * usage: setenv PATH `path++ [[new-dir] ...]`
+ *	usage: setenv PATH `path++ [[new-dir] ...]`
  */
 
 #include <stdio.h>
